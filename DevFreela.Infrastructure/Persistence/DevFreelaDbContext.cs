23305d80d1421a396ef7df1204d13aefaa39dbe7
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2021-10-06 17:42:39 -0300

    feat: create db context and db sets

--- DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
+++ DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -1,38 +1,20 @@
 using System;
 using System.Collections.Generic;
 using DevFreela.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevFreela.Infrastructure.Persistence
 {
-    public class DevFreelaDbContext
+    public class DevFreelaDbContext : DbContext
     {
-        public DevFreelaDbContext()
+        public DevFreelaDbContext(DbContextOptions<DevFreelaDbContext> options) : base(options)
         {
-            Projects = new List<Project>
-            {
-                new Project("New web project", "Gorgeous description", 1, 1, 1000),
-                new Project("New web project 2", "Gorgeous description", 1, 1, 2000),
-                new Project("New web project 3", "Gorgeous description", 1, 1, 3000)
-            };
-
-            Users = new List<User>
-            {
-                new User("Fabrício", "fabricio@gmail.com", new DateTime(2000, 01, 01)),
-                new User("Felipe", "felipe@gmail.com", new DateTime(1995, 01, 01)),
-                new User("John", "john@gmail.com", new DateTime(1992, 01, 01)),
-            };
-
-            Skills = new List<Skill>
-            {
-                new Skill("PHP"),
-                new Skill("C#"),
-                new Skill("SQL"),
-            };
+            
         }
 
-        public List<Project> Projects { get; set; }
-        public List<User> Users { get; set; }
-        public List<Skill> Skills { get; set; }
-        public List<ProjectComment> ProjectComments { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<ProjectComment> ProjectComments { get; set; }
     }
 }
