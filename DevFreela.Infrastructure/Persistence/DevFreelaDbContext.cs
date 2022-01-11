9d2820af21a16546d7a1063b413fedb485728397
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2022-01-10 21:26:58 -0300

    refact(infrastructure): change how db configurations are made

--- DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
+++ DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using DevFreela.Core.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
@@ -10,7 +11,6 @@ namespace DevFreela.Infrastructure.Persistence
     {
         public DevFreelaDbContext(DbContextOptions<DevFreelaDbContext> options) : base(options)
         {
-            
         }
 
         public DbSet<Project> Projects { get; set; }
@@ -20,48 +20,7 @@ namespace DevFreela.Infrastructure.Persistence
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Project>()
-                .HasKey(p => p.Id);
-
-            modelBuilder.Entity<Project>()
-                .HasOne(p => p.Freelancer)
-                .WithMany(f => f.FreelanceProjects)
-                .HasForeignKey(p => p.IdFreelancer)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<Project>()
-                .HasOne(p => p.Client)
-                .WithMany(f => f.OwnedProjects)
-                .HasForeignKey(p => p.IdClient)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<ProjectComment>()
-                .HasKey(p => p.Id);
-
-            modelBuilder.Entity<ProjectComment>()
-                .HasOne(p => p.Project)
-                .WithMany(p => p.Comments)
-                .HasForeignKey(p => p.IdProject);
-
-            modelBuilder.Entity<ProjectComment>()
-                .HasOne(p => p.User)
-                .WithMany(p => p.Comments)
-                .HasForeignKey(p => p.IdUser);
-
-            modelBuilder.Entity<Skill>()
-                .HasKey(s => s.Id);
-
-            modelBuilder.Entity<User>()
-                .HasKey(u => u.Id);
-
-            modelBuilder.Entity<User>()
-                .HasMany(u => u.Skills)
-                .WithOne()
-                .HasForeignKey(u => u.IdSkill)
-                .OnDelete(DeleteBehavior.Restrict);
-            
-            modelBuilder.Entity<UserSkill>()
-                .HasKey(u => u.Id);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
     }
 }
