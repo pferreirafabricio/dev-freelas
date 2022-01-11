8a3a17920d3b1bbd57f6b3ad295d53f1ec82dd33
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2022-01-10 21:50:46 -0300

    feat: add mysql connection and migration configuration

--- DevFreela.API/Startup.cs
+++ DevFreela.API/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,7 +30,10 @@ namespace DevFreela.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<DevFreelaDbContext>();
+            var connectionString = Configuration.GetConnectionString("DevFreelaCs");
+            services.AddDbContext<DevFreelaDbContext>(
+                options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+            );
             services.AddScoped<IProjectService, ProjectService>();
 
             services.AddControllers();
